05b8381fe79fbeccb467a37ecc2627de72dfc60f
iam-manibabu-karnam <91973342+iam-manibabu-karnam@users.noreply.github.com>
2025-07-09 22:19:09 +0530

    Update sst.config.ts

--- sst.config.ts
+++ sst.config.ts
@@ -1,35 +1,23 @@
-import { SSTConfig } from "sst";
 import { NextjsSite, Bucket } from "sst/constructs";
 
 export default {
-  config(_input) {
+  config(_app) {
     return {
-      name: "nextjs-s3-upload",
-      region: "us-east-1",
+      name: "car-mech-sst-deploy",
+      region: "ap-south-1",
     };
   },
-  stacks(app) {
-    app.stack(({ stack }) => {
-      // S3 bucket for file uploads
-      const uploadBucket = new Bucket(stack, "UploadBucket", {
-        cors: true
-      });
 
-      // Next.js app deploy
-      const site = new NextjsSite(stack, "NextSite", {
+  stacks(app) {
+    app.stack(function SiteStack({ stack }) {
+      const bucket = new Bucket(stack, "Uploads");
+
+      new NextjsSite(stack, "Site", {
         path: ".",
         environment: {
-          NEXT_PUBLIC_UPLOAD_BUCKET: uploadBucket.bucketName
-        }
-      });
-
-      // Permissions for app to write to S3
-      site.attachPermissions([uploadBucket]);
-
-      stack.addOutputs({
-        SiteURL: site.url,
-        UploadBucket: uploadBucket.bucketName
+          UPLOAD_BUCKET_NAME: bucket.bucketName,
+        },
       });
     });
-  }
-} satisfies SSTConfig;
+  },
+};
